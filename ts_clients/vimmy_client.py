37d41149f5ba3cc6cb8846abada76f4321aa020d
lil-il <ilyaprokofyev@mail.ru>
2021-04-24 20:44:56 +0700

    [FCTOOLSDEV-96] white/black lists in vimmy

--- ts_clients/vimmy_client.py
+++ ts_clients/vimmy_client.py
@@ -47,4 +47,30 @@ class VimmyClient(TrafficSourceClient):
 
     def add_zones_to_list(self, campaign_id, zones_list, api_key, list_type=None,
         list_to_add=None, client_key=None):
-       NotImplemented
+
+        requests_url = self._base_requests_url + f'campaigns/{campaign_id}'
+
+        get_response = requests_manager.get(requests_url, api_key=api_key)
+
+        if not isinstance(get_response, requests.Response):
+            return f'Error occurred while trying to change campaign status in Vimmy: {get_response}'
+
+        if get_response.status_code != 200:
+            return f'Non-success status code occurred while trying to ' \
+                   f'change campaign status in Vimmy: {get_response.content}'
+
+        campaign_data = get_response.json()
+        campaign_data['sites']['is_white'] = list_type == WHITELIST
+        campaign_data['sites']['items'] = zones_list
+
+        response = requests_manager.put(requests_url,
+                                        data=json.dumps(campaign_data))
+
+        if not isinstance(response, requests.Response):
+            return f'Error occurred while trying to change campaign status in Vimmy: {response}'
+
+        if response.status_code != 200:
+            return f'Non-success status code occurred while trying to ' \
+                   f'change campaign status in Vimmy: {response.content}'
+
+        return 'OK'
